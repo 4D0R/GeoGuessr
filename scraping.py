653b81542236133e8d3d7aa8bbefa4bcfb4710c6
Rohan Krishnan <rohankrishnan@live.com>
2023-04-25 16:59:13 -0400

    scrape the world

--- scraping.py
+++ scraping.py
@@ -3,7 +3,9 @@ from selenium.webdriver.common.by import By
 from constants import countries
 from pathlib import Path
 from time import sleep
-from multiprocessing import Pool
+# from multiprocessing import Pool
+import threading
+import logging
 import os
 from google.cloud import storage
 
@@ -15,8 +17,79 @@ bucket = storage_client.get_bucket("geoguessr-imgs")
 
 def main():
     # Multiprocessing
-    with Pool() as pool:
-        pool.map(scrape_country, countries.keys())
+    threads = list()
+    for index in range(10):
+        x = threading.Thread(target=scrape_world, args=(index,))
+        threads.append(x)
+        x.start()
+
+    for index, thread in enumerate(threads):
+        thread.join()
+
+
+def scrape_world(args):
+    # Create a directory for the results
+    results = Path("./data/scraped_images")
+    os.makedirs(results, exist_ok=True)
+
+    # Create a new webdriver
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options)
+
+    driver.get(f"https://randomstreetview.com/")
+    driver.set_window_size(800, 450)
+    sleep(1)
+
+    for id in [
+        "controls",
+        "map_canvas",
+        "adnotice",
+        "ad",
+        "address",
+        "minimaximize",
+        "share",
+    ]:
+        driver.execute_script(f"document.getElementById('{id}').style.opacity = 0;")
+    for className in ["gm-fullscreen-control", "intro_splash", "gmnoprint"]:
+        driver.execute_script(
+            f"Array.prototype.map.call(document.getElementsByClassName('{className}'), x => x.style.opacity = 0);"
+        )
+    
+    index = 0
+    num_scraped = 0
+    while num_scraped < 1000:
+        location_data = driver.execute_script(f"return randomLocations.all")
+        if (index >= len(location_data)):
+            driver.refresh()
+            index = 0
+            sleep(3)
+            continue
+        
+        country = location_data[index]['formatted_address'].split(",")[-1].strip()
+        # Create a directory for results
+        country_path = Path(country)
+        os.makedirs(results / country_path, exist_ok=True)
+        
+        file_name = f"{location_data[index]['lat']}x{location_data[index]['lng']}"
+        index += 1
+
+        if not os.path.isfile(results / country_path / f"{file_name}.jpg"):
+            sleep(3)
+            for className in ["gm-style"]:
+                driver.execute_script(
+                    f"Array.prototype.map.call(document.getElementsByClassName('{className}'), x => x.style.display = 'flex');"
+                )
+            driver.save_screenshot(results / country_path / f"{file_name}.jpg")
+            
+            blob = bucket.blob(f"{FOLDER}/{country_path} /{file_name}.jpg")
+            blob.upload_from_filename(results / country_path / f"{file_name}.jpg")
+            
+            driver.find_element(By.ID, "next").click()
+        num_scraped += 1
+
+    # Close driver
+    driver.close()
 
 
 def scrape_country(country_code):
